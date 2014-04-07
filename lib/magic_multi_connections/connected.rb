174b1f4a0d7634b55ca3acf9da3a6f1140bf670d
Bivek <bardan.rana@gmail.com>
2014-04-07 16:00:47 +0545

    removed unnecessary methods as it was throwing exception

--- lib/magic_multi_connections/connected.rb
+++ lib/magic_multi_connections/connected.rb
@@ -2,23 +2,23 @@ module MagicMultiConnection::Connected
   def self.included(base)
     base.instance_eval do
       alias :pre_connected_const_missing :const_missing
-      
+
       def namespace_reflections_mirror_db
         @namespace_reflections_mirror_db
       end
-  
+
       def namespace_reflections_mirror_db=(value)
         if value
           warn "DEPRACATION WARNING: Automatic namespace associations will be removed in the next major release of this gem. Please use explicit association statments."
         end
         @namespace_reflections_mirror_db = value
       end
-  
+
       def const_missing(const_id)
         # return pre_connected_const_missing(const_id) rescue nil
         target_class = "#{self.parent_module}::#{const_id}".constantize rescue nil
         return super if target_class.nil?
-        
+
         # The code below is used to solve an issue with the acts_as_versioned
         # plugin.  Because the acts_as_versioned plugin creates a 'Version' model
         # in the namespace of the class itself, the mmc method const_missing() will
@@ -31,7 +31,7 @@ module MagicMultiConnection::Connected
           def self.mmc_owner
             @@mmc_owner
           end
-        end        
+        end
         klass.establish_connection self.connection_spec
         klass
       end
@@ -39,28 +39,28 @@ module MagicMultiConnection::Connected
       def create_class(class_name, superclass = Object, &block)
         klass = Class.new superclass, &block
         result = self.const_set(class_name, klass)
-        
+
         # Cycle through all reflections, and redefine those reflections
         # for all relationships where the ActiveRecord Object follows one
-        # of the two following conditions: 
+        # of the two following conditions:
         # 1. The module is set to mirror relationships for the namespace, the reflection object is in
         #    the reflection owner's namespace and the mirror_db_connection object is not explicitly set
         # 2. The reflections mirror_db_connection is explicitly set to true.
         parent_mod = self.parent_module
         superclass.reflections.each do |reflection_name, reflection|
-          
+
           # First do a check to see if code is using a depracated method
           # of associations:
           reflection_mirrors_db = false
           if reflection.mirror_db_connection == true
             reflection_mirrors_db = true
-          elsif (reflection.mirror_db_connection == :default && 
+          elsif (reflection.mirror_db_connection == :default &&
                  namespace_reflections_mirror_db &&
                  Regexp.new("(::|^)#{parent_mod.to_s}(::|$)") =~ reflection.klass.to_s)
             warn "DEPRECATION WARNING: Automatic namespace associations will be removed in the next major release of this gem. Please use explicit association statments."
             reflection_mirrors_db = true
           end
-          
+
           if reflection_mirrors_db
             # redefine the reflection
             new_class_name = reflection.class_name.sub(parent_mod.to_s, "::#{self.name}").gsub("::::", "::")
@@ -70,21 +70,14 @@ module MagicMultiConnection::Connected
             when :has_one, :has_many, :belongs_to
               new_options[:foreign_key] ||= reflection.primary_key_name
             when :has_and_belongs_to_many
-              
+
             end
-            
+
             klass.send reflection.macro, reflection_name, new_options
-          end         
+          end
         end
-        result      
+        result
       end
-    end
-    base.extend(ClassMethods)
-  end
-  
-  module ClassMethods
-    def establish_connection_on(klass)
-      klass.establish_connection self.connection_spec
     end
   end
 end
